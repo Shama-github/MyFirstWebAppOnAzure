c6851b275b5d53563df39c7a81b0b84e525561b4
Shaik <895228@cognizant.com>
2022-05-04 17:00:00 +0530

    first commit

---
+++ Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MyFirstWebAppOnAzure.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyFirstWebAppOnAzure.WebApiApplication" Language="C#" %>
